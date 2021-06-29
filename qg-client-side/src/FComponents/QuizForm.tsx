9b2a70012561c83df435e801956add9039a9b409
hamsaravi0 <hamsaravi00@gmail.com>
2021-06-29 00:53:24 -0400

    Added an last name input

--- qg-client-side/src/FComponents/QuizForm.tsx
+++ qg-client-side/src/FComponents/QuizForm.tsx
@@ -2,17 +2,19 @@ import React from "react";
 import { useInput } from "./useInput";
 
 export const QuizForm: React.FC = () => {
-    const { value, bind, reset } = useInput('');
+    const firstNameInput= useInput('');
+    const lastNameInput = useInput('');
 
     const handleSubmit = (e : React.ChangeEvent<HTMLFormElement>) => {
         e.preventDefault();
-        alert(`Submitting Name ${value}`);
-        reset();
+        alert(`Submitting Name ${firstNameInput.value}, ${lastNameInput.value}`);
+        firstNameInput.reset(); lastNameInput.reset();
     }
 
     return (
         <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="First name" {...bind}/>
+            <input type="text" placeholder="First name" {...firstNameInput.bind}/>
+            <input type="text" placeholder="Last name" {...lastNameInput.bind}/>
             <input type="submit" value="Submit" />
         </form>
     )
